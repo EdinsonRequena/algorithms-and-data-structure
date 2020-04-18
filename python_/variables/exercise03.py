e01fbed542327f29d0b3daff44de0c75bb3eb367
EdinsonRequena <edinsonrequena@protonmail.com>
2020-04-18 15:23:52 -0400

    adding logic function to exercise03.py

--- python_/variables/exercise03.py
+++ python_/variables/exercise03.py
@@ -30,7 +30,7 @@ def cli():
 
     game4 = input("What's the game?: ")
     time4 = float(input(f"How many hours did you play {game4}?: "))
-    price4 = int(input(f"How many dollars did you spend in the {game4}?: "))
+    price4 = float(input(f"How many dollars did you spend in the {game4}?: "))
 
     print("-------------------------------------------------------------------")
 
@@ -39,6 +39,16 @@ def cli():
     price5 = float(input(f"How many dollars did you spend in the {game5}?: "))
 
     print("-------------------------------------------------------------------")
+
+
+def logic():
+    global total
+    global time1, time2, time3, time4, time5
+    global price1, price2, price3, price4, price5
+
+    total = (time1/price1 + time2/price2 + time3/price3 + time4/price4 + time5/price5) / 5
+    print(f"The games {game1}, {game2}, {game3}, {game4}, {game5} cost you {total} the hour")
+  
 
 
 def main():
