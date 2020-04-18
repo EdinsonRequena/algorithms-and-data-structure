07f20cd83acc0249793f725c99befbede5fcb7f9
EdinsonRequena <edinsonrequena@protonmail.com>
2020-04-18 15:10:28 -0400

    adding global variables in exercise03.py

--- python_/variables/exercise03.py
+++ python_/variables/exercise03.py
@@ -1,6 +1,14 @@
+game1, game2, game3, game4, game5 = str, str, str, str, str
+time1, time2, time3, time4, time5 = float, float, float, float, float
+price1, price2, price3, price4, price5 = float, float, float, float, float
+
+def cli():
+    pass
+
 
 def main():
-    pass
+    cli()
+    logic()
 
 if __name__ == '__main__':
     main()
