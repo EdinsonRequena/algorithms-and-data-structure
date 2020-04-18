bfa90eee81974cd1d39adf4fe0919eb41c628c44
EdinsonRequena <edinsonrequena@protonmail.com>
2020-04-18 14:58:12 -0400

    update exercise02.py

--- python_/variables/exercise02.py
+++ python_/variables/exercise02.py
@@ -9,21 +9,31 @@ def main():
     time1 = float(input(f"How many hours did you play {game1}?: "))
     price1 = float(input(f"How many dollars did you spend in the {game1}?: "))
 
+    print("-------------------------------------------------------------------")
+
     game2 = input("What's the game?: ")
     time2 = float(input(f"How many hours did you play {game2}?: "))
     price2 = float(input(f"How many dollars did you spend in the {game2}?: "))
+
+    print("-------------------------------------------------------------------")
 
     game3 = input("What's the game?: ")
     time3 = float(input(f"How many hours did you play {game3}?: "))
     price3 = float(input(f"How many dollars did you spend in the {game3}?: "))
 
+    print("-------------------------------------------------------------------")
+
     game4 = input("What's the game?: ")
     time4 = float(input(f"How many hours did you play {game4}?: "))
     price4 = int(input(f"How many dollars did you spend in the {game4}?: "))
 
+    print("-------------------------------------------------------------------")
+
     game5 = input("What's the game?: ")
     time5 = float(input(f"How many hours did you play {game5}?: "))
     price5 = float(input(f"How many dollars did you spend in the {game5}?: "))
+
+    print("-------------------------------------------------------------------")
 
     total = (time1/price1 + time2/price2 + time3/price3 + time4/price4 + time5/price5) / 5
     print(f"The games {game1}, {game2}, {game3}, {game4}, {game5} cost you {total} the hour")
