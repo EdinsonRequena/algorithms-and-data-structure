6a20c7979480daa107f06d94656d9a5c04e2b31d
EdinsonRequena <edinsonrequena@protonmail.com>
2020-04-23 20:38:09 -0400

    adding example00.c

---
+++ C/arithmetic-operators/example00.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int x=1, y=2, z=3;
+ 
+    x -= z;
+    y /= x;
+    z %= y;
+    printf("x=%d\ty=%d\tz=%d\n", x, y, z);
+  
+    x += y*y;
+    y *= x+z;
+    z -= y-x*x;
+    printf("x=%d\ty=%d\tz=%d\n", x, y, z);
+ 
+    x %= y *= z += x;
+    printf("x=%d\ty=%d\tz=%d\n", x, y, z);
+   
+    return 0;
+}
