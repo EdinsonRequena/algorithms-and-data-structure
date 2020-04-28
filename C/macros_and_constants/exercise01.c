03a723425278575d420579a808ace72a118fd0a3
EdinsonRequena <edinsonrequena@protonmail.com>
2020-04-28 16:54:25 -0400

    update docstring in exercise01.c

--- C/macros_and_constants/exercise01.c
+++ C/macros_and_constants/exercise01.c
@@ -5,7 +5,7 @@ Write a program that calculates the user's weight on other planets.
 For example:
 
 Enter your weight: 71.5
-Your weight on Earth is: 179.114796 kg.
+Your weight on Jupiter is: 179.114796 kg.
 Your weight on the moon is: 11.83 kg.
 Your weight on Mars is: 27.85 kg.
 Your weight on Jupiter is: 188.76 kg.
