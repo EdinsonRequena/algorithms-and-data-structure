d6e65c8ee9214b67d636854a739f9ff906af159c
EdinsonRequena <edinsonrequena@protonmail.com>
2020-04-27 21:27:07 -0400

    adding exercise01.c

---
+++ C/macros_and_constants/exercise01.c
@@ -0,0 +1,16 @@
+/*
+
+Write a program that calculates the user's weight on other planets.
+
+For example:
+
+Enter your weight: 71.5
+Your weight on Earth is: 71.50 kg.
+Your weight on the moon is: 11.83 kg.
+Your weight on Mars is: 27.85 kg.
+Your weight on Jupiter is: 188.76 kg.
+Your weight on the planet Miller: is 92.95 kg.
+Your weight in Pandora is: 57.20 kg.
+Your weight in Vulcano is: 100.10 kg.
+
+*/
