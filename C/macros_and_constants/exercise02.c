adaf4eb2bb10e9dcf3d6716ef83aab50e7ea9ea1
EdinsonRequena <edinsonrequena@protonmail.com>
2020-04-28 16:59:37 -0400

    update exercise02.c

--- C/macros_and_constants/exercise02.c
+++ C/macros_and_constants/exercise02.c
@@ -1,5 +1,4 @@
 /*
-
 Write a program that calculates the user's weight on other planets.
 
 For example:
@@ -17,12 +16,14 @@ Your weight in Mercury is: 100.10 kg.
 
 #include <stdio.h>
 
-#define LUNA 0.1655
-#define MARTE 0.3895
-#define JUPITER 2.640
-#define MILLER 1.3
-#define PANDORA 0.8
-#define VULCANO 1.4
+#define JUPITER 24.55
+#define VENUS 8.87
+#define URANO 8.887
+#define MARTE 3.71
+#define TIERRA 9.8
+#define SATURNO 10.44
+#define NEPTUNO 11.15
+#define MERCURIO 3.7
 
 int main() {
     double peso;
@@ -31,12 +32,12 @@ int main() {
     scanf("%lf", &peso);
    
 	printf("Tu peso en la Tierra es %.2lf kg.\n", peso);
-	printf("Tu peso en la luna es %.2lf kg.\n", peso*LUNA);
+	printf("Tu peso en la venus es %.2lf kg.\n", peso*VENUS);
 	printf("Tu peso en Marte es %.2lf kg.\n", peso*MARTE);
 	printf("Tu peso en Júpiter es %.2lf kg.\n", peso*JUPITER);
-	printf("Tu peso en el planeta Miller es %.2lf kg.\n", peso*MILLER);
-	printf("Tu peso en Pandora es %.2lf kg.\n", peso*PANDORA);
-	printf("Tu peso en Vulcano es %.2lf kg.\n", peso*VULCANO);
+	printf("Tu peso en el saturno es %.2lf kg.\n", peso*SATURNO);
+	printf("Tu peso en neptuno es %.2lf kg.\n", peso*NEPTUNO);
+	printf("Tu peso en mercurio es %.2lf kg.\n", peso*MERCURIO);
     
     return 0;
 }
