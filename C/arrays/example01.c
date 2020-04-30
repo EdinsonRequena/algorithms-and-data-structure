0f374018e2d764a42a2861e8f860b2c7bf8e5df5
EdinsonRequena <edinsonrequena@protonmail.com>
2020-04-30 16:14:52 -0400

    adding example01.c

---
+++ C/arrays/example01.c
@@ -0,0 +1,18 @@
+/*
+
+Inicializando tablas
+
+*/
+
+#include <stdio.h>
+
+#define TABLE1 3
+
+int main() {
+
+    int datos[TABLE1] = {5, 10, 8}; // Esta tabla contiene 3 elementos donde el elemento 0 es el entero 5
+                                    // el elemento 1 es el 10 y el elemento 2 es el 8
+    
+
+    return 0;
+}
