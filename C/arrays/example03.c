af2f7ceca955a0ee4707592d59beb95a36c4742a
EdinsonRequena <edinsonrequena@protonmail.com>
2020-05-01 18:30:29 -0400

    update example03.c

--- C/arrays/example03.c
+++ C/arrays/example03.c
@@ -12,9 +12,9 @@ int main() {
 
     datos[2] = (datos[0] + datos[1])*2;
 
-    printf("The first element on the array is: %d\n", datos[0]);
-    printf("The thrid element on the array is: %d\n", datos[2]);
-    printf("The number entered by the user was: %d", datos[1]);
+    printf("The first element on the array is: %d\n", datos[0]); // Imprime el primer elemento del array que es 5
+    printf("The thrid element on the array is: %d\n", datos[2]); // Imprime el tercer elemento del array que es el resultado de la operacion de la linea 13
+    printf("The number entered by the user was: %d", datos[1]); // Imprime el segundo elemento del array que es el introducido por el usuario
 
     return 0;
 }
