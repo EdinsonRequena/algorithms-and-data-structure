d6749f27fbb5572c73f4a98f798812037cd073d9
EdinsonRequena <edinsonrequena@protonmail.com>
2020-05-04 13:24:41 -0400

    adding coments for example08.c

--- C/arrays/example08.c
+++ C/arrays/example08.c
@@ -9,8 +9,10 @@ Cadena de caracteres en arrays
 #define TAM 8
 
 int main() {
-    char nombre[TAM] = {'A', 'n', 'a'};
-    char nombre1[TAM] = "Eddy";
+    char nombre[TAM] = {'A', 'n', 'a'}; // Elemento a elemento
+    char nombre1[TAM] = "Eddy"; // Conjunto
+
+//  todos los del array que no se especifican automaticamente con el codigo ASCII 0 (NULL)
 
     printf("Hola, %s.\n", nombre);
     printf("Hola, %s.", nombre1);
