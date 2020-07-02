8bf9fe195052aeab5d85be33aaf8532fff3b60f6
EdinsonRequena <edinsonrequena@protonmail.com>
2020-07-01 23:33:58 -0400

    update

--- platzi-algoritmos/C/user-define-data-type.c
+++ platzi-algoritmos/C/user-define-data-type.c
@@ -7,3 +7,32 @@ Profesor: Ricardo Celis
 Alumno: @edinsonrequena
 
 */
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+struct client
+{
+    char name[50];
+    char id[10];
+    float credit;
+    char address[100];
+};
+
+int main()
+{
+    struct client client1 = {0};
+    strcpy(client1.name , "Andrea Vinas");
+    strcpy(client1.id , "0000000001");
+    client1.credit = 1000000;
+    strcpy(client1.address , "Colombia, Boyaca");
+
+    printf("The client name is: %s \n", client1.name);
+    printf("The client Id is: %s \n", client1.id);
+    printf("The client Credit is: %f \n", client1.credit);
+    printf("The client Address is: %s \n", client1.address);
+
+
+    return 0;
+}
